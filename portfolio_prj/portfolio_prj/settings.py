730234d073871c7ee06509caa0fd019c59bb97eb
SubMax <subbbbbbbbject@gmail.com>
2020-09-17 10:42:18 +0500

    Начальный шаблон

--- portfolio_prj/portfolio_prj/settings.py
+++ portfolio_prj/portfolio_prj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'investinfo'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'portfolio_prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['portfolio_prj/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
