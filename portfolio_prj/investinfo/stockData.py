fdcfda9249cd0f87daee9516bbb4df2edaf37861
SubMax <subbbbbbbbject@gmail.com>
2020-09-21 21:02:37 +0500

    получение основной информации для инструментов

---
+++ portfolio_prj/investinfo/stockData.py
@@ -0,0 +1,24 @@
+from yfinance import Ticker
+'''
+Модуль для работы с yfinance, для получения информации об инвестиционных инструментах
+
+'''
+
+
+def getStockData(tickerName):
+    """
+    :param tickerName: краткое название биржевого инструмента
+    :return: возвращает список из [тикера, название компании, описание компании, ссылка на лого]
+    """
+
+    info = []
+    ticker = Ticker(tickerName).info
+    info.append(ticker.get('symbol'))
+    info.append(ticker.get('longName'))
+    info.append(ticker.get('longBusinessSummary'))
+    info.append(ticker.get('logo_url'))
+    return info
+
+
+if __name__ == "__main__":
+    print(getStockData('msft'))
