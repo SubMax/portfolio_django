642a1bca0976cd923a5af79d48ddb8e93c20eff4
SubMax <subbbbbbbbject@gmail.com>
2020-10-06 16:50:39 +0500

    тип в переменных в адресе

--- portfolio_prj/investinfo/urls.py
+++ portfolio_prj/investinfo/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path(r'about', views.about, name='about'),
     path(r'<slug:ticker>', views.description_ticker, name='ticker_info'),
     path(r'<slug:ticker>/<str:text>/<slug:period>/<slug:interval>/', views.stock_data_ticker, name='chart_period'),
-    path(r'<slug:ticker>/<str:text>/<str:ystart>/<str:mstart>/<str:dstart>/'
-         r'<str:yend>/<str:mend>/<str:dend>/<slug:interval>/',
+    path(r'<slug:ticker>/<str:text>/<int:ystart>/<int:mstart>/<int:dstart>/'
+         r'<int:yend>/<int:mend>/<int:dend>/<slug:interval>/',
          views.stock_data_ticker,
          name='chart_date'),
 ]
