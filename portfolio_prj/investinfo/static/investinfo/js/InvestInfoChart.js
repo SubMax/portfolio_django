538542e951dc8583ba8898f46c1202e6d36f09d5
SubMax <subbbbbbbbject@gmail.com>
2020-10-17 17:49:54 +0500

    вынес создание Chart в отдельный скрипт

---
+++ portfolio_prj/investinfo/static/investinfo/js/InvestInfoChart.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: date,
+        datasets: [{
+            data: adjclose,
+            label: "{{ title }}",
+            borderColor: "#3e95cd",
+            fill: false
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Скорректированная цена закрытия акции'
+                },
+            legend: {
+                position: 'top'
+            },
+            scales: {
+                xAxes: [{
+                    ticks:{
+                        maxRotation: 0,
+                        autoSkipPadding: 50,
+                        },
+                    }],
+                yAxes: [{
+                    ticks:{
+                        padding: 1,
+                        lineHeight: 10
+                        },
+                    }]
+                }
+        }
+    });
+
+function heightCanvas(){
+}
